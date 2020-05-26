7affde32629ae51269b2d992ab0abafafe44910b
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-05-26 14:35:29 +0530

    selected elements values

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded',() => {
-
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const grid = document.querySelector('.grid');
+    const ScoreDisplay = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start-button');
+    const Width = 10;
     
 })
