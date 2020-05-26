3797f9a5329e1bc53898302cf1a0798d1e48a931
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-05-26 19:02:20 +0530

    added game over function

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ document.addEventListener('DOMContentLoaded',() => {
             draw();
             displayShape();
             addScore();
+            gameOver();
         }
     }
 
@@ -201,6 +202,14 @@ document.addEventListener('DOMContentLoaded',() => {
                 squares = squaresRemoved.concat(squares);
                 squares.forEach(cell => grid.appendChild(cell));
             }
+        }
+    }
+
+    // game over
+    function gameOver() {
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            scoreDisplay.innerHTML = 'end';
+            clearInterval(timerId);
         }
     }
 
