b2b858f3c69e1f06613a6329b5b23c8b82a24b29
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-05-26 16:26:48 +0530

    added move left & right function

--- app.js
+++ app.js
@@ -59,12 +59,75 @@ document.addEventListener('DOMContentLoaded',() => {
     }
 
     // undraw tetromino
-    function draw() {
+    function undraw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.remove('tetromino');
         })
     }
 
+    // move the 
+    timerId = setInterval(moveDown, 1000);
 
+    // assign function to keyCodes
+    function control(e) {
+        if(e.keyCode === 37) {
+            moveLeft();
+        } else if (e.keyCode === 38) {
+            //rotate();
+        } else if (e.keyCode === 39) {
+            moveRight();
+        } else if (e.keyCode === 40) {
+            moveDown();
+        }
+    }
+    document.addEventListener('keyup',control);
+
+    // move function
+    function moveDown() {
+        undraw();
+        currentPosition += GRID_WIDTH;
+        draw();
+        freeze();
+    }
+
+    // freeze function
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + GRID_WIDTH].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList.add('taken'));
+            // start new tetromino
+            random = Math.floor(Math.random()*theTetrominoes.length);
+            current = theTetrominoes[random][currentRotation];
+            currentPosition = 4;
+            draw();
+        }
+    }
+
+    // move the tetromino left unless it is at the edge
+    function moveLeft() {
+        undraw();
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % GRID_WIDTH === 0);
+
+        if(!isAtLeftEdge) currentPosition -= 1;
+
+        if(current.some(index => squares[currentPosition + index].classList.add('taken'))) {
+            currentPosition++;
+        }
+        
+        draw();
+    }
+
+    // move the tetromino right unless it is at the edge
+    function moveRight() {
+        undraw();
+        const isAtRightEdge = current.some(index => (currentPosition + index) % GRID_WIDTH === GRID_WIDTH - 1);
+
+        if(!isAtRightEdge) currentPosition += 1;
+
+        if(current.some(index => squares[currentPosition + index].classList.add('taken'))) {
+            currentPosition--;
+        }
+        
+        draw();
+    }
 
 })
